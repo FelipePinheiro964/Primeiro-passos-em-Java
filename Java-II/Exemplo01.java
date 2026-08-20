278aa4986f36c0e826ff0bce222a2e0772691984
Felipe Pinheiro <felipe.fossa@edu.pucrs.br>
2026-08-20 20:38:46 -0300

    Exemplo de if e else basico

---
+++ Java-II/Exemplo01.java
@@ -0,0 +1,30 @@
+public class Exemplo01 {
+  public static void main(String[] args) {
+    double N1, N2, N3, N4, RES;
+    
+    System.out.print("Informe a nota do primeiro bimestre: ");
+    N1 = Double.parseDouble(System.console().readLine());
+
+    System.out.print("Informe a nota do segundo bimestre: ");
+    N2 = Double.parseDouble(System.console().readLine());
+
+    System.out.print("Informe a nota do terceior bimestre: ");
+    N3 = Double.parseDouble(System.console().readLine());
+
+    System.out.print("Informe a nota do quarto bimestre: ");
+    N4 = Double.parseDouble(System.console().readLine());
+
+
+    System.out.print("Sua nota media: ");
+    RES = (N1 + N2 + N3 + N4) / 4;
+
+    System.out.println(RES);
+
+    // If e else para aluno aprovado/reprovado
+
+    if(RES >= 7)
+      System.out.println("Aluno aprovado!");
+    else
+      System.out.println("Aluno reprovado.");
+  }
+}

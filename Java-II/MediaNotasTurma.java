e05a96b6f7af543561f89374c643cd2b0f8fd0a2
Felipe Pinheiro <felipe.fossa@edu.pucrs.br>
2026-08-21 00:57:20 -0300

    Pos-incrementos e pre-incrementos

--- Java-II/MediaNotasTurma.java
+++ Java-II/MediaNotasTurma.java
@@ -12,14 +12,14 @@ public class MediaNotasTurma {
     soma = 0;
     // mais um exemplo de atualização do codigo com o for, eu achei interessante, da pra usar em alguns lugares
     // agora mais algumas modificacoes, deixando o codigo melhor para leitura
-    for(notaslidas = 0; notaslidas < nro_estudante; notaslidas = notaslidas += 1) {
+    for(notaslidas = 0; notaslidas < nro_estudante; notaslidas++) {// Operador de pos-incremento
       System.out.printf("Nota %d: ", notaslidas + 1);
       nota = Integer.parseInt(System.console().readLine());
       soma =+ nota;
       }
     
-
-    media = (double) soma / nro_estudante;
+    
+    media = (double)soma / nro_estudante;
     System.out.printf("Media das notas = %.1f\n", media);
 
 

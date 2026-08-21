585a1821b86cd1070475b9bfccc63e69428106d6
Felipe Pinheiro <felipe.fossa@edu.pucrs.br>
2026-08-20 23:33:19 -0300

    BigInteger para fatorial

--- Java-II/Fatorial.java
+++ Java-II/Fatorial.java
@@ -1,18 +1,21 @@
+import java.math.BigInteger;
+
 public class Fatorial {
 
   public static void main(String[] args) {
 
-    int numero, valor, fatorial;
+    int numero, valor;
+    BigInteger fatorial;
 
     System.out.print("Informe um numero inteiro: ");
     numero = Integer.parseInt(System.console().readLine());
 
     valor = 2;
-    fatorial = 1;
+    fatorial = BigInteger.ONE;
 
     while (valor <= numero) {
 
-      fatorial = fatorial * valor;
+      fatorial = fatorial.multiply(BigInteger.valueOf(valor));
       valor = valor + 1;
 
     }

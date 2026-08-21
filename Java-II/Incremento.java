91c8547b06611516e126c3cdc73ae38aeab3bebb
Felipe Pinheiro <felipe.fossa@edu.pucrs.br>
2026-08-21 01:01:17 -0300

    Pos-incrementos e pre-incrementos testando com visualização

---
+++ Java-II/Incremento.java
@@ -0,0 +1,24 @@
+public class Incremento {
+
+  public static void main(String[] args) {
+    
+    int c;
+
+    c = 5;
+
+    System.out.println(c); // mostra o 5
+    System.out.println(c++); // mostra 5 depois
+    // incrementa
+    System.out.println(c); // mostra 6
+    System.out.println(); // so pra pula linha
+    
+    c = 5;
+
+    System.out.println(c); // mostra o 5
+    System.out.println(++c); //pre-incrementa depois
+    System.out.println(c); // mostra 6
+
+
+  } 
+
+}

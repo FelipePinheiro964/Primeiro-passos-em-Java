78e0ea6e7f6b8666452a041b25961c354754c2df
Felipe Pinheiro <felipe.fossa@edu.pucrs.br>
2026-08-21 00:41:34 -0300

    Atualização de MediaNotasTurma.java (obs: Esqueci COMPLETAMENTE de usar feat entre outros no inicio dos commits, agora vou manter ate o proximo reposttorio)

--- Java-II/MediaNotasTurma.java
+++ Java-II/MediaNotasTurma.java
@@ -10,17 +10,13 @@ public class MediaNotasTurma {
     nro_estudante = Integer.parseInt(System.console().readLine());
 
     soma = 0;
-
-    notaslidas = 0;
-
-    while(notaslidas < nro_estudante) {
-
+    // mais um exemplo de atualização do codigo com o for, eu achei interessante, da pra usar em alguns lugares
+    for(notaslidas = 0; notaslidas < nro_estudante; notaslidas = notaslidas + 1) {
       System.out.printf("Nota %d: ", notaslidas + 1);
       nota = Integer.parseInt(System.console().readLine());
       soma = soma + nota;
-      notaslidas = notaslidas + 1;
-
-    }
+      }
+    
 
     media = (double) soma / nro_estudante;
     System.out.printf("Media das notas = %.1f\n", media);
